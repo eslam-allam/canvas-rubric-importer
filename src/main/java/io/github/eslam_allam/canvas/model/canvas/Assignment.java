900dc137ab2a35ff8892e27c9060be824989ae78
Eslam Allam <eslamallam73@gmail.com>
2026-01-04 13:32:10 +0300

    fix: Update assignment points update request to send correct values

--- src/main/java/io/github/eslam_allam/canvas/model/canvas/Assignment.java
+++ src/main/java/io/github/eslam_allam/canvas/model/canvas/Assignment.java
@@ -3,6 +3,8 @@ package io.github.eslam_allam.canvas.model.canvas;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -186,6 +188,11 @@ public record Assignment(
 
         public String type() {
             return this.name().toLowerCase();
+        }
+
+        @JsonValue
+        public String toValue() {
+            return this.type();
         }
     }
 }
--- src/main/java/io/github/eslam_allam/canvas/request/AssignmentPointsUpdateRequest.java
+++ src/main/java/io/github/eslam_allam/canvas/request/AssignmentPointsUpdateRequest.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.eslam_allam.canvas.model.canvas.Assignment.GradingType;
 
 public record AssignmentPointsUpdateRequest(
-        @JsonProperty("points_possible") double pointsPossible,
+        @JsonProperty("points_possible") Double pointsPossible,
         @JsonProperty("grading_type") GradingType gradingType) {}

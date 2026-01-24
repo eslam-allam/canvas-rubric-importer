eaafc6a6e8de7d8218e5aafb2ee78386659a3b22
Eslam Allam <eslamallam73@gmail.com>
2026-01-24 17:36:02 +0300

    feat: Add Inter font

--- src/main/java/io/github/eslam_allam/canvas/gui/CanvasRubricImporter.java
+++ src/main/java/io/github/eslam_allam/canvas/gui/CanvasRubricImporter.java
@@ -1,5 +1,7 @@
 package io.github.eslam_allam.canvas.gui;
 
+import java.util.Objects;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import io.github.eslam_allam.canvas.client.CanvasCredentialProvider;
@@ -22,17 +24,17 @@ import io.github.eslam_allam.canvas.viewmodel.ConnectionPanelVM;
 import io.github.eslam_allam.canvas.viewmodel.ListPaneVM;
 import io.github.eslam_allam.canvas.viewmodel.StatusLabelVM;
 import jakarta.inject.Singleton;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 @Singleton
-@Component(
-        modules = {
-            ListPaneFactory.class,
-            SceneSwitcherFactory.class,
-            NotificationModule.class,
-            CanvasCredentialProviderModule.class,
-            ControllerModule.class
-        })
+@Component(modules = {
+        ListPaneFactory.class,
+        SceneSwitcherFactory.class,
+        NotificationModule.class,
+        CanvasCredentialProviderModule.class,
+        ControllerModule.class
+})
 public interface CanvasRubricImporter {
     CanvasCredentialProvider canvasCredentialProvider();
 
@@ -64,12 +66,43 @@ public interface CanvasRubricImporter {
                 @BindsInstance Stage primaryStage, @BindsInstance PreferencesService preferencesService);
     }
 
-    default void init() {
+    default void init() throws IllegalStateException {
+        loadResources();
+
         statusLabel().bind(statusLabelVM());
         connectionPanelController();
         coursesPaneController();
         assignmentsPaneController();
         rubricConfigurationController();
         mainController().initAndShow();
+    }
+
+    private void loadResources() throws IllegalStateException {
+        String[] fontFiles = {
+                "Inter_28pt-Black.ttf",
+                "Inter_28pt-BlackItalic.ttf",
+                "Inter_28pt-Bold.ttf",
+                "Inter_28pt-BoldItalic.ttf",
+                "Inter_28pt-ExtraBold.ttf",
+                "Inter_28pt-ExtraBoldItalic.ttf",
+                "Inter_28pt-ExtraLight.ttf",
+                "Inter_28pt-ExtraLightItalic.ttf",
+                "Inter_28pt-Italic.ttf",
+                "Inter_28pt-Light.ttf",
+                "Inter_28pt-LightItalic.ttf",
+                "Inter_28pt-Medium.ttf",
+                "Inter_28pt-MediumItalic.ttf",
+                "Inter_28pt-Regular.ttf",
+                "Inter_28pt-SemiBold.ttf",
+                "Inter_28pt-SemiBoldItalic.ttf",
+                "Inter_28pt-Thin.ttf",
+                "Inter_28pt-ThinItalic.ttf",
+        };
+
+        for (String file : fontFiles) {
+            Objects.requireNonNull(Font.loadFont(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/fonts/Inter/" + file),
+                    file + " font not found on class path."), 12), "Failed to load " + file + " font");
+        }
     }
 }
